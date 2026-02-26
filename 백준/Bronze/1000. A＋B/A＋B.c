c92c4869499b552b4b0376029e463b28b17e8b0a
dladyddn133-creator <dladyddn133@gmail.com>
2026-02-26 10:34:18 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+int main(void)
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    int result =0;
+    result = A+B;
+    printf("%d",result);
+
+    return 0;
+}
