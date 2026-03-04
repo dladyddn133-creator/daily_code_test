01628515b8e94ad17853e9f8e3d1e17fbd3fe878
dladyddn133-creator <dladyddn133@gmail.com>
2026-03-04 09:13:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+int main(void)
+{
+    char alpha;
+
+    scanf("%c", &alpha);
+    printf("%d", (int)alpha);
+    return 0;
+}
